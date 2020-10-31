ed3b2116f162fbb0f0ed34a4b4f12e31b9b29204
igihozo-stella <igihozo96@gmail.com>
2020-10-31 10:02:45 +0200

    create class and use id and name in that class.

--- Angular-apps/Quotes/src/app/app.component.ts
+++ Angular-apps/Quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-  constructor(){
-    this.quotes=['"Be yourself; everyone else is already taken."','“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”']
-  }
+  quotes:Quote[]=[
+    {id:1,name:'"Be yourself; everyone else is already taken."'},
+    {id:2,name:'“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”'},
+    {id:3,name:'“Don’t walk behind me; I may not lead. Don’t walk in front of me; I may not follow. Just walk beside me and be my friend.”'},
+    {id:4,name:'“Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind.”'},
+    {id:5,name:'“No one can make you feel inferior without your consent.”'},
+  ];
+  
   
 }
