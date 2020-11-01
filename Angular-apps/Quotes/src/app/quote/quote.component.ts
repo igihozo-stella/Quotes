6b85b4509c41addf6a6dd7c39d10de76c09f767b
igihozo-stella <igihozo96@gmail.com>
2020-11-01 17:32:35 +0200

    add the publisher in quote component.

--- Angular-apps/Quotes/src/app/quote/quote.component.ts
+++ Angular-apps/Quotes/src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1,'"Be yourself; everyone else is already taken."','Oscar Wilde',new Date(2020,10,27)),
-    new Quote(2,'“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”','Dr.Seus',new Date(2020,11,1)),
-    new Quote(3,'“Don’t walk behind me; I may not lead. Don’t walk in front of me; I may not follow. Just walk beside me and be my friend.”','Albert Camus',new Date(2020,10,31)),
-    new Quote(4,'“Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind.”','Bernard M. Baruch',new Date(2020,10,30)),
-    new Quote(5,'“No one can make you feel inferior without your consent.”','Eleanor Roosevelt',new Date(2020,11,1)),
+    new Quote(1,'"Be yourself; everyone else is already taken."','Oscar Wilde','Cyrus Kent',new Date(2020,10,27)),
+    new Quote(2,'“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”','Dr.Seus','Kamal Neo',new Date(2020,11,1)),
+    new Quote(3,'“Don’t walk behind me; I may not lead. Don’t walk in front of me; I may not follow. Just walk beside me and be my friend.”','Albert Camus','Naomi Camp',new Date(2020,10,31)),
+    new Quote(4,'“Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind.”','Bernard M. Baruch','Jerome Grant',new Date(2020,10,30)),
+    new Quote(5,'“No one can make you feel inferior without your consent.”','Eleanor Roosevelt','Rose Maine',new Date(2020,11,1)),
   ];
   toggleDetails (index){
     this.quotes[index].showDescription=!this.quotes[index].showDescription;
